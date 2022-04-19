d4dacdfe6e4cbecd0eeadf6aaa9523e1da4af754
dennisshushack <dennis.shushack@uzh.ch>
2022-04-19 21:35:32 +0200

    Update new_sampler_50tmp.sh

--- monitors/monitor2/new_sampler_50tmp.sh
+++ monitors/monitor2/new_sampler_50tmp.sh
@@ -17,7 +17,7 @@ desiredSamples=99999
 timeAcumulative=0
 
 #	Final and temporal output files
-finalOutput="/tmp/monitors/m2_$(date +'%Y-%m-%d-%H-%M')_${timeWindowSeconds}s"
+finalOutput="/tmp/monitors/m2/m2_$(date +'%Y-%m-%d-%H-%M')_${timeWindowSeconds}s"
 tempOutput=temp
 
 ##############################################################
