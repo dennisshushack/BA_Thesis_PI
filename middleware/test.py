7fd410c5de9ab65a4a22bb960e6d2dc7fd9a27c0
dennis <dennis.shushack@uzh.ch>
2022-05-30 15:30:47 +0200

    Test file

---
+++ middleware/test.py
@@ -0,0 +1,20 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+# Create a POST request on 192.168.0.249:5000/rest/training with Basic Auth:
+# username: admin
+# password: admin
+# and data: {device = 000000005426d851 category = training ml_type = anomaly monitors = ['m1', 'm2', 'm3'] behavior = normal path =/home/dennis/Documents/data/000000005426d851/anomaly/training}
+localhost = '192.168.0.249:5000'
+username = 'admin'
+password = 'admin'
+serial = '000000005426d851'
+category = 'training'
+ml_type = 'anomaly'
+monitors = ['m1', 'm2', 'm3']
+behavior = 'normal'
+path = '/home/dennis/Documents/data/000000005426d851/anomaly/training'
+response = requests.post("http://{localhost}/rest/train".format(localhost=localhost), auth=HTTPBasicAuth('admin', 'admin'), json={"ml_type": ml_type, "monitors": monitors, "behavior": behavior, "category": category, "path": path, "device": serial})
+
+
+
