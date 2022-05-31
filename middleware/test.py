b37fba888ac12b7c47814643233c17fd03333160
dennis <dennis.shushack@uzh.ch>
2022-05-31 18:45:42 +0200

    add

--- middleware/test.py
+++ middleware/test.py
@@ -9,11 +9,11 @@ localhost = '192.168.0.249:5000'
 username = 'admin'
 password = 'admin'
 serial = '000000005426d851'
-category = 'training'
+category = 'testing'
 ml_type = 'anomaly'
 monitors = ['m1', 'm2', 'm3']
-behavior = 'normal'
-path = '/home/dennis/Documents/data/000000005426d851/anomaly/training'
+behavior = 'ransom1'
+path = '/home/dennis/Documents/data/000000005426d851/anomaly/testing'
 response = requests.post("http://{localhost}/rest/train".format(localhost=localhost), auth=HTTPBasicAuth('admin', 'admin'), json={"ml_type": ml_type, "monitors": monitors, "behavior": behavior, "category": category, "path": path, "device": serial})
 
 
